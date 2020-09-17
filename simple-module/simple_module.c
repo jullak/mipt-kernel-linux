f549fa58fbbdbb87f78f231a60f2edcc4e767548
Julia <julia-akzhigitova@yandex.ru>
2020-09-17 15:09:30 +0300

    add read/write in driver

--- simple-module/simple_module.c
+++ simple-module/simple_module.c
@@ -6,6 +6,7 @@
 #include <linux/module.h>
 #include <linux/list.h>
 #include <linux/slab.h>
+#include <linux/uaccess.h>
 
 #include <linux/string.h>
 
@@ -14,7 +15,7 @@ MODULE_AUTHOR("Julia Akzhigitova");
 MODULE_DESCRIPTION("Phonebook module");
 MODULE_VERSION("0.01");
 
-#define CHAR_BUF_LEN 256
+#define CHAR_BUF_LEN 64
 
 typedef struct {
   uint32_t age;
@@ -22,18 +23,18 @@ typedef struct {
   char surname[CHAR_BUF_LEN];
   char email[CHAR_BUF_LEN];
   char phone[CHAR_BUF_LEN];
-} user_entry_t;
+} user_data_t;
 
 
 typedef struct {
   struct list_head next_list;
-  user_entry_t user;
+  user_data_t user;
 } phonebook_t;
 
 static struct list_head phonebook_head;
 
 
-int find_user_entry_by_surname(const char * surname, phonebook_t ** returned) {
+int find_user_data_by_surname(const char * surname, phonebook_t ** returned) {
   printk(KERN_INFO "Start searching user with surname %s \n", surname);
 
   struct list_head * iter;
@@ -53,35 +54,35 @@ int find_user_entry_by_surname(const char * surname, phonebook_t ** returned) {
   return -1;
 }
 
-int add_user_entry(const char * name, const char * surname, const char * email, const char * phone, uint32_t age) {
-  printk(KERN_INFO "Start adding user with surname %s \n", surname);
+int add_user_data(user_data_t * entry) {
+  printk(KERN_INFO "Start adding user with surname %s \n", entry->surname);
 
   
   phonebook_t * find_user = NULL;
-  int res = find_user_entry_by_surname(surname, &find_user);
-  if (-1 == res) {
+  int res = find_user_data_by_surname(entry->surname, &find_user);
+  if (0 == res) {
   	printk(KERN_INFO "Surname already exists");
   	return -1;
   }
 
   phonebook_t * new_user = kmalloc(sizeof(phonebook_t), GFP_KERNEL);
 
-  new_user->user.age = age;
-  strncpy(new_user->user.name, name, CHAR_BUF_LEN);
-  strncpy(new_user->user.surname, surname, CHAR_BUF_LEN);
-  strncpy(new_user->user.email, email, CHAR_BUF_LEN);
-  strncpy(new_user->user.phone, phone, CHAR_BUF_LEN);
+  new_user->user.age = entry->age;
+  strncpy(new_user->user.name, entry->name, CHAR_BUF_LEN);
+  strncpy(new_user->user.surname, entry->surname, CHAR_BUF_LEN);
+  strncpy(new_user->user.email, entry->email, CHAR_BUF_LEN);
+  strncpy(new_user->user.phone, entry->phone, CHAR_BUF_LEN);
 
   INIT_LIST_HEAD(&new_user->next_list);
 
   list_add(&new_user->next_list, &phonebook_head);
 
-  printk(KERN_INFO "Added user with surname %s \n", surname);
+  printk(KERN_INFO "Added user with surname %s \n", entry->surname);
 
   return 0;
 }
 
-int del_user_entry(phonebook_t * deleted) {
+int del_user_data(phonebook_t * deleted) {
   printk(KERN_INFO "Start deleting user with surname %s \n", deleted->user.surname);
 
   list_del(&deleted->next_list);
@@ -93,6 +94,8 @@ int del_user_entry(phonebook_t * deleted) {
 }
 
 void delete_all(void) {
+  printk(KERN_INFO "Free list \n");
+
   struct list_head * iter;
   phonebook_t * entry;
     
@@ -105,7 +108,6 @@ void delete_all(void) {
   	}
 }
 
-//--проверки на exist!--
 //отдать юзера полностью?
 //функции для устройства - разнести их с самой структурой
 /*-------------------------------------------------------------*/
@@ -113,10 +115,18 @@ void delete_all(void) {
 #define CLASS_NAME "phonebook_class"
 #define DEV_NAME "phonebook_dev"
 
+#define ADD_CMD "add "
+#define GET_CMD "get "
+#define DEL_CMD "del "
+
+static const int CMD_LEN = 4;
+
 static dev_t dev_type;
 static struct cdev ph_cdev;
 static struct class * phonebook_class;
 static struct device * phonebook_device;
+
+char returned[CHAR_BUF_LEN];
 
 
 static int ph_open(struct inode * inode, struct file * file) {
@@ -132,11 +142,65 @@ static int ph_close(struct inode * inode, struct file * file) {
 }
 
 static ssize_t ph_read(struct file * file, char __user * user_buffer, size_t size, loff_t * offset) {
-  return 0;
+  if (*offset > 0) return 0;
+  
+  if (0 == copy_to_user(user_buffer, returned, strlen(returned))) {
+  	(*offset) += strlen(returned);
+  	return strlen(returned);
+  }
+  return -1;
 }
 
 static ssize_t ph_write(struct file * file, const char __user * user_buffer, size_t size, loff_t * offset) {
-  return 0;
+  char buffer[CMD_LEN + sizeof(user_data_t)];
+  memset(buffer, 0, CMD_LEN + sizeof(user_data_t));
+
+  if (size > CMD_LEN + sizeof(user_data_t)) {
+  	printk(KERN_INFO "Too large request \n");
+  	strcpy(returned, "too large\0");
+  	return -1;
+  }
+  copy_from_user(buffer, user_buffer, size);
+
+
+  if (strncmp(ADD_CMD, buffer, CMD_LEN) == 0) {
+  	user_data_t adding_user;
+  	//name, surname, email, phone, age
+  	sscanf(buffer + CMD_LEN, "%s %s %s %s %u", 
+  		adding_user.name, adding_user.surname, adding_user.email, adding_user.phone, &adding_user.age);
+
+  	int res = add_user_data(&adding_user);
+  	if (-1 == res) {
+  	  strcpy(returned, "already exist\0");
+  	  return -1;
+  	}
+
+  	strcpy(returned, "done\0");
+  } else if (strncmp(GET_CMD, buffer, CMD_LEN) == 0) {
+  	phonebook_t * entry;
+  	if (0 > find_user_data_by_surname(buffer + CMD_LEN, &entry)) {
+  	  strcpy(returned, "not found\0");
+  	  return -1;
+  	}
+
+  	//name, surname, email, phone, age
+  	sprintf(returned, "%s %s %s %s %u", 
+  		entry->user.name, entry->user.surname, entry->user.email, entry->user.phone, entry->user.age);
+  } else if (strncmp(DEL_CMD, buffer, CMD_LEN) == 0) {
+  	phonebook_t * entry;
+  	if (0 > find_user_data_by_surname(buffer + CMD_LEN, &entry)) {
+  	  strcpy(returned, "not found\0");
+  	  return -1;
+  	}
+
+  	del_user_data(entry);
+  	strcpy(returned, "done\0");
+  } else {
+  	strcpy(returned, "bad command\0");
+  	return -1;
+  }
+
+  return size;
 }
 
 
